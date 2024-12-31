d7b6cbfb55e913705f0a6b055b5f1cd0485fd01f
Paweł Protaś <pawciopr@gmail.com>
2024-12-31 11:45:46 +0100

    Add schedule function implementation

---
+++ homework/schedule/schedule.cpp
@@ -0,0 +1,18 @@
+﻿#include "schedule.hpp"
+
+#include <thread>
+
+void schedule(const std::function<void()>& func, const std::chrono::seconds duration) {
+    std::this_thread::sleep_for(duration);
+    func();
+}
+
+void schedule(const std::function<void(int)>& func, const std::chrono::seconds duration, const int intValue) {
+    std::this_thread::sleep_for(duration);
+    func(intValue);
+}
+
+void schedule(const std::function<void(std::string, double)>& func, const std::chrono::seconds duration, const std::string& stringValue, const double doubleValue) {
+    std::this_thread::sleep_for(duration);
+    func(stringValue, doubleValue);
+}
---
+++ homework/schedule/schedule.hpp
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <chrono>
+#include <functional>
+#include <string>
+
+void schedule(const std::function<void()>& func, std::chrono::seconds duration);
+void schedule(const std::function<void(int)>& func, std::chrono::seconds duration, int intValue);
+void schedule(const std::function<void(std::string, double)>& func, std::chrono::seconds duration, const std::string& stringValue, double doubleValue);
